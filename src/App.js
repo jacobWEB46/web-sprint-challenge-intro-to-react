cbd369d6689ffbbb0bba056cb82e57897fc47dd2
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-08-13 15:21:45 -0400

    changes
    finished creating styled and character

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import data from './mocks/handlers.js'
+import Character from  './components/Character.js'
+import { set } from 'msw/lib/types/context';
+export const BASE_URL = 'https://swapi.dev/api/people'
 
 const App = () => {
+  const [peopleDate, setPeopleData] = useState(data)
+
+  const changeName () => {
+    setPeopleData({...setPeopleData, name: "Puke"})
+  }
+
+  useEffect(() => {
+    console.log(peopleDate)
+    axios.get(`${BASE_URL}/1/`)
+    .then(res => {
+      console.log(res)
+      console.log(peopleDate);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +33,12 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+return (
+  <div className="App">
+    <h1 className="Header">Characters</h1>
+     <Character peopleData={peopleDate}></Character>
+  </div>
+);
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+
+import React, { useEffect } from 'react';
+import { BASE_URL } from '../App.js';
+import StyleWars from './Styled.js'
+
+
+
+const Character = props => {
+    const { peopleData } = props
+
+    return (
+        <div className="characters">
+
+            {peopleData.map((data, index) => (
+                <StyleWars key={index}>
+                    <h3>{data.name}</h3>
+                    <p>{data.birth_year}</p>
+                </StyleWars>
+            ))}
+        </div>
+    )
+
+}
+
+export default Character;
